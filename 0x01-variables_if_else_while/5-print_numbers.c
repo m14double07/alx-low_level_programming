82d482816664ba6e4a5024584497b6a00d884163
bunshii <m14double07@gmail.com>
2023-06-16 18:03:08 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+*main - Enrty point to program
+*Return: 0 at success
+*/
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+		i++;
+
+	printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+
+
+}
