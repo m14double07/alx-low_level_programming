87dc477adb6852bc6ef305a20a35f9bb9403dfb8
bunshii <m14double07@gmail.com>
2023-06-17 16:33:15 -0700

    added new line to no 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,15 +12,15 @@ int main(void)
 
 	int  ch[5] = {1, 4, 4, 8, 4};
 
-	printf("Size of char: %d byte(s)", ch[0]);
+	printf("Size of char: %d byte(s)\n", ch[0]);
 
-	printf("Size of int: %d byte(s)", ch[1]);
+	printf("Size of int: %d byte(s)\n", ch[1]);
 
-	printf("Size of long int: %d byte(s)", ch[2]);
+	printf("Size of long int: %d byte(s)\n", ch[2]);
 
-	printf("Size of long long int: %d byte(s)", ch[3]);
+	printf("Size of long long int: %d byte(s)\n", ch[3]);
 
-	printf("Size of float: %d byte(s)", ch[4]);
+	printf("Size of float: %d byte(s)\n", ch[4]);
 
 	return (0);
 }
