35843ed563fbaf9b436f3588fb19f67c63cbc2fd
bunshii <m14double07@gmail.com>
2023-06-19 12:54:38 -0700

    almost done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main -Entry point into the programm
+*Return: 0 at success
+*/
+
+int main(void)
+
+{
+	fputs(stderr, "and that piece of art is useful"
+	 - Dora Korpar, 2015-10-19 \n");
+	return (0);
+}
