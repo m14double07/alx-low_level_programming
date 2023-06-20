c22d608087017c670bb343987757dabb736ce29e
bunshii <m14double07@gmail.com>
2023-06-20 12:31:32 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point into the program
+*Return: Always success
+*/
+
+
+int main(void)
+
+{
+
+
+	printf("_putchar");
+	printf("\n");
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project is ment to practice more in c programming
