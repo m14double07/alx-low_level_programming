ada8350c3cb515794f66fd548faad7130dbdf830
bunshii <m14double07@gmail.com>
2023-06-21 16:55:23 -0700

    c nesting

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*islower - This programm checks if a charactere is lowercase
+*/
+
+
+int _islower(int c)
+
+
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+
+}
