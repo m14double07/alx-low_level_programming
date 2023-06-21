34ff85cde6a30b4acc4aabb6c21676469ad0e1f1
bunshii <m14double07@gmail.com>
2023-06-21 12:06:28 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - Print all lowercase alphabet 10x
+*/
+
+void print_alphabet_x10(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	_putchar(ch);
+
+	_putchar('\n');
+	}
+}
