0586990adcb0a058f0ff1ccf7c4e29e137bcee1c
bunshii <m14double07@gmail.com>
2023-06-26 17:17:07 -0700

    light

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*swap_int - Entry to programm
+*@a: First int
+*@b: Second int
+*/
+
+void swap_int(int *a, int *b)
+
+{
+	int j;
+
+	j = *a;
+	*a = *b;
+	*b = j;
+
+
+
+}
