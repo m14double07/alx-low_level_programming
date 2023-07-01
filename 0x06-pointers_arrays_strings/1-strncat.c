50d9516f43374e2eff39aa9328082823a996e8c6
bunshii <m14double07@gmail.com>
2023-07-01 05:59:57 -0700

    strncat & strcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*_strncat - Concatinates two strings
+*@dest: Destination of concatination
+*@src: Merge
+*@n: Character to be concatinated
+*Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+	i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+
+	i++;
+	j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+#endif
