db3a1f75dd26dc7017e64a023769d5448b1ba7dd
bunshii <m14double07@gmail.com>
2023-07-03 16:44:30 -0700

    fix task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
 *_strchr - Entry to programm
@@ -17,5 +16,5 @@ char *_strchr(char *s, char c)
 	return (s);
 	s++;
 	}
-	return (NULL);
+	return ('\0');
 }
