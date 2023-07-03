c02297b3ce24db899c2bcaf572bcb49ae7309a29
bunshii <m14double07@gmail.com>
2023-07-03 06:41:12 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+*_memcpy - Copy strings
+*@dest: Destination of coppied string
+*@src: Sorce of string to be copied
+*@n: Value of character to be copied
+*Return: dest
+*/
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
