26db0b02b269fa3e8146fd651f81bae4839f2967
bunshii <m14double07@gmail.com>
2023-07-17 13:04:49 -0700

    task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,13 @@ struct dog
 	char *owner;
 };
 
-void first_dog(struct dog *d, char *name, int age, char *owner);
-void print_dog(struct dog *d);
+/**
+*dog_t - Changed name to dog_t
+*/
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
