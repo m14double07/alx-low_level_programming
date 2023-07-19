3b52f6c876c7f9452423300628fea6f87cba0571
bunshii <m14double07@gmail.com>
2023-07-18 17:34:11 -0700

    sum task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int  main()
+{
+	char *c= "__file__";
+
+	_putchar(*c);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef ABS
+
+#define ABS(x)
+
+#endif
