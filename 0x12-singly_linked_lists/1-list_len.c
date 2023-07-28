17633784975aa51df7aeefb33b4cb5cd3c13dd86
m14double07 <bunshii07@gmail.com>
2023-07-27 17:15:26 -0700

    task 2

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* list_len - Prints number of elements in list
+* @h: The list
+* Return: the number of nodes.
+*/
+
+size_t list_len(const list_t *h)
+{
+   int c = 0;
+
+
+   while (h)
+   {
+       count++;
+       h = h->next;
+   }
+   return (c);
+}
+
