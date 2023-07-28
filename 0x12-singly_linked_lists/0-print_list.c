aaa4c51b5dd978c4ae27eab856355f8b310510cb
m14double07 <bunshii07@gmail.com>
2023-07-27 17:01:28 -0700

    task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+*print_list - Prints list of items
+*@h: Pointer to elements
+*Return: The number of elements
+*/
+
+
+
+size_t print_list(const list_t *h)
+{
+	int c = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+		printf("[0] (nil)");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
