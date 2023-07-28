1cbf8e4ce106b35ed75e70fff0a26dd4c23024f8
m14double07 <bunshii07@gmail.com>
2023-07-28 07:28:03 -0700

    last task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,17 +3,17 @@
 
 
 /**
-*main - prints the minimum number of coins to make change for an amount of money
+*main - prints the minimum num of coins to make change
 *@argc: number of arguments
 *@argv: array of arguments
 *Return: 0 (Success), 1 (Error)
 */
-		
+
 int main(int argc, char *argv[])
-{		
+{
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
-		
+
 	if (argc != 2)
 	{
 	printf("Error\n");
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	}
 	num = atoi(argv[1]);
 	result = 0;
-		
+
 	if (num < 0)
 	{
 		printf("0\n");
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 	{
 		while (num >= coins[j])
 		{
-		result++;		
+		result++;
 		num -= coins[j];
 		}
 	}
