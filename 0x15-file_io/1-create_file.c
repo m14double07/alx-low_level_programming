050ab48f06af6fa7c5bd999e74369d854ae85a38
m14double07 <m14double07@gmial.com>
2023-11-02 02:40:07 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,30 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, let, re;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT, O_WRONLY, O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
 
-	for (re = 0; text_content[re]; re++)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-	let = write(fd, text_content, re);
+	rwr = write(fd, text_content, nletters);
 
-	if (let == -1)
+	if (rwr == -1)
 		return (-1);
 
 	close(fd);
 
 	return (1);
-
 }
